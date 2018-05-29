21328c69128c633cc01fd7d5cf2f86daf832ff4c
Leann Kim <leannminakim@gmail.com>
2018-05-29 03:01:12 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def my_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  my_two_array = [1,2]
+end
+
+def first_element
+  arr = ["taylor_swift", "eminem", 4]
+  arr[0]
+end
+
+def third_element
+  arr = ["rar", "eminem", "taylor_swift"]
+  arr[2]
+end
+
+def last_element
+  arr_last = ["rar", "eminem", "taylor_swift"]
+  arr[-1]
+end
+
+def first_element_with_array_methods (array)
+  array.first
+end
+
+def last_element_with_array_methods (array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
