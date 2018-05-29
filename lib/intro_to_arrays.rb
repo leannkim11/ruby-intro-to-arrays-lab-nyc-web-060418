f8a829083e17cc0ca5cc6418cd6ac18aa44c7dd4
Leann Kim <leannminakim@gmail.com>
2018-05-29 02:54:20 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,18 @@
+my_new_array = new.Array
+
+my_two_array = [1,2]
+
+def my_first_element
+  arr = ["taylor_swift", "eminem", 4]
+  arr[0]
+end
+
+def my_third_element
+  arr = ["rar", "eminem", "taylor_swift"]
+  arr[2]
+end
+
+def my_last_element
+  arr_last = ["rar", "eminem", "taylor_swift"]
+  arr[-1]
+end
